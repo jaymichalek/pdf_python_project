c3c6fffff431e10fca4643e1cd77bc1d9bf2b1cd
hashtagd3v <jaycoinminer@gmail.com>
2022-03-02 17:56:26 -0600

    Reading and writing PDF files using PyPDF2 module

---
+++ pdf.py
@@ -0,0 +1,10 @@
+import PyPDF2
+
+with open('pdf-playground/dummy.pdf', 'rb') as file:
+    reader = PyPDF2.PdfFileReader(file)
+    page = reader.getPage(0)
+    page.rotateCounterClockwise(90)
+    writer = PyPDF2.PdfFileWriter()
+    writer.addPage(page)
+    with open('pdf-playground/tilt.pdf', 'wb') as new_file:
+        writer.write(new_file)
